00365b3d6996a1fb456e84bf521a84c4c9f059d5
bharath-raghavan <bharath.rn@gmail.com>
2025-02-19 14:37:03 -0500

    Fixed vel generation

--- enflow/data/transforms.py
+++ enflow/data/transforms.py
@@ -6,6 +6,8 @@ from ..units.conversion import ang_to_lj, amu_to_lj, kelvin_to_lj
 
 import numpy as np
 from rdkit import Chem
+from scipy.special import erf
+from scipy.interpolate import interp1d as interp
 
 class ConvertPositions(BaseTransform):
     def __init__(self, input_unit):
@@ -22,15 +24,34 @@ class RandomizeVelocity(BaseTransform):
     def __init__(self, temp):
         self.kBT = kelvin_to_lj(temp)
         self.pse = Chem.GetPeriodicTable()
+        
+    def generate_velocities(self, n):
+        """ generate a set of velocity vectors in 3D from the MB inverse CDF function """
+        """ Adapted from: https://github.com/tpogden/quantum-python-lectures/blob/master/11_Monte-Carlo-Maxwell-Boltzmann-Distributions.ipynb """
+        
+        # Cumulative Distribution function of the Maxwell-Boltzmann speed distribution
+        m = 1
+        a = np.sqrt(self.kBT/m)
+        MB_CDF = lambda v : erf(v/(np.sqrt(2)*a)) - np.sqrt(2/np.pi)* v* np.exp(-v**2/(2*a**2))/a
+        v = np.arange(0,2500,0.1)
+        cdf = MB_CDF(v)
+
+        #create interpolation function to CDF
+        inv_cdf = interp(cdf,v)
+                                 
+        rand_nums = np.random.random(n)
+        speeds = inv_cdf(rand_nums)
+    
+        theta = np.arccos(np.random.uniform(-1,1,n))
+        phi = np.random.uniform(0,2*np.pi,n)
+    
+        # convert to cartesian units
+        vx = speeds * np.sin(theta) * np.cos(phi) 
+        vy = speeds * np.sin(theta) * np.sin(phi)
+        vz = speeds * np.cos(theta)
+    
+        return np.stack([vx, vy, vz], axis=1, dtype=np.float64)
 
     def forward(self, data):        
-        m = torch.tensor([amu_to_lj(self.pse.GetAtomicWeight(i)) for i in data.z])
-
-        a = np.sqrt(self.kBT/m)
-        velx = (torch.rand(12).sum()-6)*a
-        vely = (torch.rand(12).sum()-6)*a
-        velz = (torch.rand(12).sum()-6)*a
-        
-        data.vel = torch.stack([velx, vely, velz], dim=1)
-        
+        data.vel = torch.from_numpy(self.generate_velocities(data.N))
         return data
