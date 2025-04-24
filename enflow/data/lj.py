dfadce8b70d6d115e744fb2a567665bf77dbbe51
bharath-raghavan <bharath.rn@gmail.com>
2025-04-24 15:32:01 -0400

    Updated mdengine and ljdataset

--- enflow/data/lj.py
+++ enflow/data/lj.py
@@ -1,121 +1,75 @@
-import math
 import numpy as np
-import torch
-from .base import BaseDataset, Data
-from ..utils.conversion import kelvin_to_lj, femtosecond_to_lj
+from .simulated import SimulatedDataset
+from ..utils.constants import sigma as sig, eps
 
-class LJMDEngine:
-    def __init__(self, softening, target_kBT, dt, nu, box):
-        self.softening = softening
-        self.target_kBT = target_kBT
-        self.dt = dt
-        self.nu = nu
-        self.box = box
-        
-    def compute_force(self, pos):
-        dr = (pos.unsqueeze(1) - pos)
-        r_sq = dr.pow(2).sum(dim=2)
-        r = r_sq.pow(0.5)
-        r_ = r.pow(-1)
-        r_13 = (r+math.sqrt(self.softening)).pow(-13)
-        r_7 = (r+math.sqrt(self.softening)).pow(-7)
-        f = 48*r_+(r_13 - 0.5*r_7)
-        f[f == float("Inf")] = 0
-        dx = dr[:,:,0]
-        fx = (f*dx).sum(dim=1)
-        dy = dr[:,:,1]
-        fy = (f*dy).sum(dim=1)
-        dz = dr[:,:,2]
-        fz = (f*dz).sum(dim=1)
-        return torch.stack((fx, fy, fz), dim=1)
+import openmm as mm
+import openmm.app as app
+import openmm.unit as unit
 
-    def verlet_andersen(self, x, v, f, N):
-        x += self.dt*v + 0.5*self.dt*self.dt*f
-        v += 0.5*self.dt*f
+def arrange_points_on_grid(n, box, space):
+    """Arranges n points on a 3D grid.
 
-        f = self.compute_force(x)
+    Args:
+        n: The number of points to arrange.
 
-        v += 0.5*self.dt*f
-        kBT = v.pow(2).sum()/(3*N)
+    Returns:
+        A NumPy array of shape (num_x * num_y * num_z, 3) representing the 3D grid coordinates.
+    """
+    num_z = int(np.ceil(n**(1/3)))
+    num_y = int(np.ceil((n/num_z)**(1/2)))
+    num_x = int(np.ceil(n/(num_y*num_z)))
+
+    x = np.linspace(space, box[0]-space, num_x)
+    y = np.linspace(space, box[1]-space, num_y)
+    z = np.linspace(space, box[2]-space, num_z)
+
+    xv, yv, zv = np.meshgrid(x, y, z)
     
-        sigma = math.sqrt(self.target_kBT)
-
-        randf = torch.rand(N) < self.nu*self.dt
-        v[randf] = torch.normal(0, sigma, size=v[randf].shape, dtype=torch.float64)
-
-        return x,v,kBT.item(),f
-
-    def run(self, N, nT):
-        x = (self.box[1] - self.box[0])*torch.rand(N, 3, dtype=torch.float64) + self.box[0]
-        v = torch.normal(0, math.sqrt(self.target_kBT), size=x.shape, dtype=torch.float64)
+    points = np.stack((xv.flatten(), yv.flatten(), zv.flatten()), axis=-1)
     
-        f = self.compute_force(x)
+    return points[:n]
         
-        kBTs = []
+class LJDataset(SimulatedDataset):       
+    def setup(self, **input_params):
+        N = input_params['n_atoms']
+        scale = 1
+        dist_units = input_params['dist_unit']
+        if dist_units == 'ang': scale = 0.1
+        L = np.array(input_params['box'])*scale*unit.nanometers # assume square box for now
+        softening = input_params['softening']
+        cutoff = input_params.get('cutoff', 3.0)
         
-        for i in range(nT):
-            x, v, kBT, f = self.verlet_andersen(x, v, f, N)
-            kBTs.append(kBT)
-        
-        return x, v, np.array(kBTs)
-        
-def prepare_dataset(md, params):
-    data_list = []
-    for param in params:
-        N = param[0]
-        box = param[1]
-        nT = param[2]
-        discard = param[3]
-        
-        pos, vel, kBT = md.run(N, box, nT, discard)
+        # Lennard-Jones parameters
+        sigma = sig * 1e10 * unit.angstrom
+        epsilon = eps * 1e-3 * unit.kilojoules_per_mole
 
-        data = Data(
-            h=torch.rand(N, 5, dtype=torch.float64),
-            g=torch.rand(N, 5, dtype=torch.float64),
-            pos=pos,
-            vel=vel,
-            N=N,
-            kBT=kBT 
-        )
+        # Make an empty topology
+        topology = app.Topology()
 
-        data_list.append(data)
+        # Add a single chain
+        chain = topology.addChain()
 
-    return data_list
+        # Add atoms to the topology
+        for i in range(N):
+            residue = topology.addResidue(name='Ar', chain=chain)
+            atom = topology.addAtom(name='Ar', element=app.element.get_by_symbol('Ar'), residue=residue)
+
+        topology.setPeriodicBoxVectors(np.eye(3)*L)
+
+        # Generate positions on a grid
+        positions = arrange_points_on_grid(N, L._value, 0.5)*unit.nanometers
+        
+        custom_lj = '4*epsilon*((sigma/(scale*sigma + r))^12-(sigma/(scale*sigma + r))^6); sigma=0.5*(sigma1+sigma2); epsilon=sqrt(epsilon1*epsilon2)'
+        custom_nb_force = mm.CustomNonbondedForce(custom_lj)
+
+        custom_nb_force.addPerParticleParameter('sigma')
+        custom_nb_force.addPerParticleParameter('epsilon')
+        custom_nb_force.addGlobalParameter('scale', softening)
+
+        for i in range(N):
+            custom_nb_force.addParticle([sigma, epsilon])
     
-class LJDataset(BaseDataset):       
-    def process(self, **input_params):
+        custom_nb_force.setNonbondedMethod(mm.CustomNonbondedForce.CutoffPeriodic)
+        custom_nb_force.setCutoffDistance(cutoff*sigma)
         
-        md = LJMDEngine(input_params['softening'], input_params['target_kBT'], input_params['dt'], input_params['nu'], input_params['box'])
-        log = input_params['log_file']
-        discard = input_params['discard']
-        node_nf = input_params['node_nf']
-        
-        log_txt = ''
-        
-        for i,simulation in enumerate(input_params['simulations']):
-            N = simulation[0]
-            nT = simulation[1]
-        
-            pos, vel, kBT = md.run(N, nT)
-            
-            start = int(nT*discard/100)
-            
-            mean = kBT[start:].mean()
-            var = kBT[start:].var()
-            
-            log_txt += f'**********************\nSimulation {i}:\nN={N}\nNumber of timesteps:{nT}\n'
-            log_txt += f'Temperature Mean:{mean} and Variance:{var}\nTemperature log:\n'
-            log_txt += '\n'.join(["%.2f" % number for number in kBT])
-            log_txt += '\n'
-            self.append(
-                z=['Ar']*N,
-                h=torch.rand(N, node_nf, dtype=torch.float64),
-                g=torch.rand(N, node_nf, dtype=torch.float64),
-                pos=pos,
-                vel=vel,
-                N=N,
-                label=f'Temperature Mean:{mean} and Variance:{var}\n'
-            )
-            
-        with open(log, 'w') as f:
-            f.write(log_txt)
+        return topology, positions, custom_nb_force, "LJ"
---
+++ enflow/data/simulated.py
@@ -0,0 +1,120 @@
+from sys import stdout
+from abc import ABC, abstractmethod
+import torch
+from .base import BaseDataset, Data
+
+import openmm as mm
+import openmm.app as app
+import openmm.unit as unit
+
+class SimulatedDatasetReporter(object):
+    def __init__(self, node_nf, transform, report_interval, report_from, desc, dist_units, time_units):
+        self.data_list = []
+        self.transform = transform
+        self.node_nf = node_nf
+        self.report_interval = report_interval
+        self.report_from = report_from
+        self.desc = desc
+        self.dist_units = dist_units
+        self.time_units = time_units
+
+    def describeNextReport(self, simulation):
+        steps = self.report_interval - simulation.currentStep%self.report_interval
+        return {'steps': steps, 'periodic': None, 'include':['positions', 'velocities']}
+
+    def report(self, simulation, state):
+        if simulation.currentStep < self.report_from: return
+        
+        pos = state.getPositions().value_in_unit(self.dist_units)
+        vel = state.getVelocities().value_in_unit(self.dist_units/self.time_units)
+        N = len(pos)
+        
+        data = Data(
+            z=['Ar']*N,
+            h=torch.rand(N, self.node_nf, dtype=torch.float64),
+            g=torch.rand(N, self.node_nf, dtype=torch.float64),
+            pos=torch.tensor(pos, dtype=torch.float64),
+            vel=torch.tensor(vel, dtype=torch.float64),
+            N=N,
+            label=f'Simulated dataset: {self.desc} Frame: {simulation.currentStep}'
+        )
+
+        if self.transform:
+            self.data_list.append(self.transform(data))
+        else:
+            self.data_list.append(data)
+
+class SimulatedDataset(BaseDataset, ABC):    
+    def append(self, z, h, g, pos, vel, N, label):
+        data = Data(
+            z=z,
+            h=h,
+            g=torch.zeros_like(h),
+            pos=pos,
+            vel=torch.zeros_like(pos),
+            N=N,
+            label=label
+        )
+
+        if self.transform:
+            self.data_list.append(self.transform(data))
+        else:
+            self.data_list.append(data)
+    
+    @abstractmethod
+    def setup(self, **input_params):
+        pass
+        
+    def process(self, **input_params):
+        temp = input_params['temp']
+        node_nf = input_params['node_nf']
+        report_interval = input_params['interval']
+        report_from = input_params['discard']
+        if report_from == -1: report_from = report_interval
+        log = input_params['log']
+        traj = input_params['traj']
+        n_iter = input_params['n_iter']
+        dist_units = input_params['dist_unit']
+        time_units = input_params['time_unit']
+        dt = input_params['dt']
+        friction = input_params['friction']
+        
+        if dist_units == 'ang':
+            dist_units = unit.angstrom
+        elif dist_units == 'nm':
+            dist_units = unit.nanometers
+            
+        if time_units == 'pico':
+            time_units = unit.picoseconds
+        elif time_units == 'femto':
+            time_units = unit.femtoseconds
+        
+        topology, positions, force, desc = self.setup(**input_params)
+        
+        # Create the system and add the particles, forces to it
+        system = mm.System()
+        system.setDefaultPeriodicBoxVectors(*topology.getPeriodicBoxVectors())
+        for atom in topology.atoms():
+            system.addParticle(atom.element.mass)    
+        
+        system.addForce(force)
+        
+        scale = 1
+        if time_units == 'femto': scale = 1e-3
+        
+        integrator = mm.LangevinMiddleIntegrator(temp*unit.kelvin, friction/(scale*unit.picosecond), dt*scale*unit.picoseconds)
+        simulation = app.Simulation(topology, system, integrator)
+        simulation.context.setPositions(positions)
+        simulation.context.setVelocitiesToTemperature(temp*unit.kelvin)
+        
+        # Add reporters to get data
+        rep = SimulatedDatasetReporter(node_nf, self.transform, report_interval, report_from, desc, dist_units, time_units)
+        simulation.reporters.append(rep)
+        
+        # Add reporters to output log and traj
+        simulation.reporters.append(app.PDBReporter(traj, report_interval))
+        simulation.reporters.append(app.StateDataReporter(log, report_interval, step=True, potentialEnergy=True, temperature=True))
+        simulation.reporters.append(app.StateDataReporter(stdout, report_interval, step=True, potentialEnergy=True, temperature=True))
+        print("Running MD simulation")
+        simulation.step(n_iter)
+        self.data_list = rep.data_list # capture data list from rep
