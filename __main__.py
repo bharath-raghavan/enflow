cfe702ef00bd19a7fa436e4af1192892f8c32217
David M. Rogers <predictivestatmech@gmail.com>
2025-05-02 11:39:12 -0400

    Renamed project, added pyproject and alchemist CLI.

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from enflow.main import Main
+from alchemist.main import Main
         
 if __name__ == "__main__":
     main_hndl = Main(world_size=os.environ.get('SLURM_NTASKS'), world_rank=os.environ.get('SLURM_PROCID'), local_rank=os.environ.get('SLURM_LOCALID'), num_cpus_per_task=os.environ.get("SLURM_CPUS_PER_TASK"))
