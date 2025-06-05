30540b1741a0ef60a292c3b92e589d27960d947f
David M. Rogers <predictivestatmech@gmail.com>
2025-06-05 18:37:03 -0400

    Updated alchemist driver fn.

--- alchemist/config.py
+++ alchemist/config.py
@@ -20,13 +20,17 @@ class NetworkSetup(BaseModel):
     hidden_nf: int
 
 class DynamicSetup(BaseModel):
+    cls: str = "LJDataset"
+    r_cut: float
+    report_interval: int = 10
+    report_from: int = 100
+    temp: float = 300
+    traj_file: str
+    dt: float
     integrator: str
     n_iter: int
-    dt: float
-    r_cut: float
-    box_pad: float = 1
-    checkpoint_path: str
-    network: NetworkSetup
+    #box_pad: float = 1
+    #network: NetworkSetup
 
 class LossSetup(BaseModel):
     temp: float = 298.15
@@ -44,13 +48,18 @@ class ConfigFile(BaseModel):
     training: TrainingSetup
     dataset: TrajectoryData
 
-def load_config(fname: Path) -> ConfigFile:
-    """ Read a ConfigFile from a yaml or json-formatted
-    file.
+def load_dict(fname: Path) -> ConfigFile:
+    """ Read a dict from a yaml or json-formatted file.
     """
     with open(fname, "r", encoding="utf-8") as f:
         if fname.suffix in [".yaml", ".yml"]:
             data = yaml.safe_load(f)
         else:
             data = json.load(f)
-    return ConfigFile.model_validate(**data)
+    return data
+
+def load_config(fname: Path) -> ConfigFile:
+    """ Read a ConfigFile from a yaml or json-formatted
+    file.
+    """
+    return ConfigFile.model_validate(**load_dict(fname))
