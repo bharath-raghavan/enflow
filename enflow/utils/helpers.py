e66daba01194f782edf4ab6300784cb75aa85f73
bharath-raghavan <bharath.rn@gmail.com>
2025-06-11 14:43:02 -0400

    Commond gaussian func and periodic correction

--- enflow/utils/helpers.py
+++ enflow/utils/helpers.py
@@ -1,16 +1,9 @@
+import math
 import torch
 
-ELEMENTS = { 1:'H' ,                                                                                                                                                                             2:'He',
-             3:'Li',   4:'Be',                                                                                                                 5:'B' ,   6:'C' ,   7:'N' ,   8:'O' ,   9:'F' ,  10:'Ne',
-            11:'Na',  12:'Mg',                                                                                                                13:'Al',  14:'Si',  15:'P' ,  16:'S' ,  17:'Cl',  18:'Ar',
-            19:'K' ,  20:'Ca',  21:'Sc',            22:'Ti',  23:'V' ,  24:'Cr',  25:'Mn',  26:'Fe',  27:'Co',  28:'Ni',  29:'Cu',  30:'Zn',  31:'Ga',  32:'Ge',  33:'As',  34:'Se',  35:'Br',  36:'Kr',
-            37:'Rb',  38:'Sr',  39:'Y' ,            40:'Zr',  41:'Nb',  42:'Mo',  43:'Tc',  44:'Ru',  45:'Rh',  46:'Pd',  47:'Ag',  48:'Cd',  49:'In',  50:'Sn',  51:'Sb',  52:'Te',  53:'I' ,  54:'Xe',
-            55:'Cs',  56:'Ba',  57:'La',            72:'Hf',  73:'Ta',  74:'W' ,  75:'Re',  76:'Os',  77:'Ir',  78:'Pt',  79:'Au',  80:'Hg',  81:'Tl',  82:'Pb',  83:'Bi',  84:'Po',  85:'At',  86:'Rn',
-            87:'Fr',  88:'Ra',  89:'Ac',           104:'Rf', 105:'Db', 106:'Sg', 107:'Bh', 108:'Hs', 109:'Mt', 110:'Ds', 111:'Rg', 112:'Cn', 113:'Nh', 114:'Fl', 115:'Mc', 116:'Lv', 117:'Ts', 118:'Og',
-                                          58:'Ce',  59:'Pr',  60:'Nd',  61:'Pm',  62:'Sm',  63:'Eu',  64:'Gd',  65:'Tb',  66:'Dy',  67:'Ho',  68:'Er',  69:'Tm',  70:'Yb',  71:'Lu',
-                                          90:'Th',  91:'Pa',  92:'U' ,  93:'Np',  94:'Pu',  95:'Am',  96:'Cm',  97:'Bk',  98:'Cf',  99:'Es', 100:'Fm', 101:'Md', 102:'No', 103:'Lr'
-}
-
+def log_gaussian(z):
+    return -0.5*( (z**2).sum() + math.log(2*math.pi) )
+    
 def apply_pbc(pos, box):
     return pos - (pos/box).round()*box
 
@@ -19,11 +12,21 @@ def get_box_len(pos):
     max_ = torch.max(pos, dim=0)[0]
     return (max_-min_).round()
 
-def get_periodic_images(pos, bl):
-    return torch.cat([pos+torch.tensor([a,b,c], dtype=pos.dtype, device=pos.device) for c in [-bl[2],bl[2],0] for b in [-bl[1],bl[1],0] for a in [-bl[0],bl[0],0]])
+def get_periodic_images_within(pos, box, r_cut):
+    # find all 27 periodic images of positions
+    pos_all_periodic_images = torch.cat([pos+torch.tensor([a,b,c], dtype=pos.dtype, device=pos.device) for c in [-box[2],box[2],0] for b in [-box[1],box[1],0] for a in [-box[0],box[0],0]])
     
-def wrap_ids_across_periodic_img(ids, N):
-    return ids - (ids//N)*N
+    # find positions within box and r_cut
+    ellipse_radii = box+r_cut
+    scaled_points = pos_all_periodic_images/ellipse_radii
+    ids_in_ellipse = torch.sum(scaled_points**2, axis=1) <= 1
+    pos_all_periodic_images = pos_all_periodic_images[ids_in_ellipse]
+    
+    # and get corresponding id mapping from pos to pos_all_periodic_images
+    id_mapping = torch.tensor(list(range(0, pos.shape[0])), device=pos.device).repeat(27)
+    id_mapping = id_mapping[ids_in_ellipse]
+    
+    return pos_all_periodic_images, id_mapping
     
 def get_element(elem, mass):
     if elem == '':
